b0b0c1e5fe2614f476916166886344ac0f884fcd
Ilya Slezkin <ilia.slezkin@r1.team>
2024-04-17 23:27:24 +0500

    develop: отключил все лишние зависимости, убрал неиспользуемые классы + рефакторинг

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,12 +33,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")   // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
     // Уже есть в какой то либе, но там версия с уязвимостями. Указываем принудительно:
     implementation("org.apache.commons:commons-compress:1.26.1")     // https://mvnrepository.com/artifact/org.apache.commons/commons-compress
 
-    implementation("org.liquibase:liquibase-core:4.24.0")
-    runtimeOnly("org.postgresql:postgresql:42.7.3")      // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    //implementation("org.liquibase:liquibase-core:4.24.0")
+    //runtimeOnly("org.postgresql:postgresql:42.7.3")      // https://mvnrepository.com/artifact/org.postgresql/postgresql
     runtimeOnly("com.h2database:h2:2.2.224")
 
     implementation("org.projectlombok:lombok:$lombokVersion")
--- src/main/java/com/lamardinho/sportnotifier/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lamardinho.sportnotifier.repository;
-
-import com.lamardinho.sportnotifier.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
