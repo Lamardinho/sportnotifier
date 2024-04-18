df405a5989714af2dc5205c88a611a66b434e994
Ilya Slezkin <ilia.slezkin@r1.team>
2024-04-18 22:27:07 +0500

    develop: фикс отправки сообщений после рефакторинга

--- src/main/java/com/lamardinho/sportnotifier/footballdataorg/scheduler/FootballDataOrgScheduler.java
+++ src/main/java/com/lamardinho/sportnotifier/footballdataorg/scheduler/FootballDataOrgScheduler.java
@@ -7,7 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +19,7 @@ import java.time.LocalDate;
 @ConditionalOnProperty(value = "app.football-data-org.scheduler.conditional", havingValue = "true")
 @RequiredArgsConstructor
 @Log4j2
-@Profile({"prod", "vdsina"})
+@Profile({"!test"})
 public class FootballDataOrgScheduler {
 
     @NonNull
@@ -28,7 +30,8 @@ public class FootballDataOrgScheduler {
 
     private LocalDate date;
 
-    @Scheduled(cron = "0 0 10 * * *")
+    @EventListener(ApplicationReadyEvent.class)
+    @Scheduled(cron = "0 0 5 * * *")
     public void updateToday() {
         val now = LocalDate.now();
         if (date == null || now.isAfter(date)) {
