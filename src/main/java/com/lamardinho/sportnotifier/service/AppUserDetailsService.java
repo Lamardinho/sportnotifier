48b6e7a3eb7853fc7a8ea6f90979ef7ad03a47a5
Ilya Slezkin <ilia.slezkin@r1.team>
2024-05-01 16:32:36 +0500

    develop: code refactoring

--- src/main/java/com/lamardinho/sportnotifier/service/AppUserDetailsService.java
+++ src/main/java/com/lamardinho/sportnotifier/service/AppUserDetailsService.java
@@ -1,10 +1,11 @@
 package com.lamardinho.sportnotifier.service;
 
-import com.lamardinho.sportnotifier.common.AppException;
 import com.lamardinho.sportnotifier.dtomappers.AppUserDetailsMapper;
+import com.lamardinho.sportnotifier.entity.user.AppUserDetails;
 import com.lamardinho.sportnotifier.repository.UserRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AppUserDetailsService implements UserDetailsService {
 
     @NonNull
@@ -25,8 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String login) {
         val user = userRepository
                 .findByLogin(login)
-                .orElseThrow(() -> new AppException(String.format("User '%s' not found", login)));
-
+                .orElse(new AppUserDetails()); // не бросаем тут исключение "User not found" чтобы не дать злоумышленникам осуществлять подбор логинов
         return detailsMapper.toDto(user);
     }
 }
