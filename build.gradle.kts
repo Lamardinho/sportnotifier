d0362147fad573afa99103fc7fad7b29b1d894cd
Marcus <slezkin23@gmail.com>
2024-05-08 22:26:46 +0500

    postgres_add: postgres add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,26 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-gradle-plugin
 var springBootVersion: String = "3.2.4"
+
 var lombokVersion: String = "1.18.30"
-var springWeb: String = "6.1.6"
-val openUiWebMvc = "2.5.0"
-val apacheCommonsCompress = "1.26.1"
-val h2Db = "2.2.224"
+
+// https://mvnrepository.com/artifact/org.springframework/spring-web
+var springWebVersion: String = "6.1.6"
+
+// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
+val openUiWebMvcVersion = "2.5.0"
+
+// https://mvnrepository.com/artifact/org.apache.commons/commons-compress
+val apacheCommonsCompressVersion = "1.26.1"
+
+val h2DbVersion = "2.2.224"
+
+// https://mvnrepository.com/artifact/org.postgresql/postgresql
+val postgresqlVersion = "42.7.3"
+
 val thymeleafVersion = "3.1.2.RELEASE"
+
 val mapstructVersion = "1.4.2.Final"
 
 plugins {
@@ -34,27 +48,27 @@ repositories {
 
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion") // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-gradle-plugin
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     // todo: Уже есть в какой то либе, но версия с уязвимостями. Указываем принудительно более новую:
-    implementation("org.springframework:spring-web:$springWeb") // https://mvnrepository.com/artifact/org.springframework/spring-web
+    implementation("org.springframework:spring-web:$springWebVersion")
 
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openUiWebMvc")   // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openUiWebMvcVersion")
     // Уже есть в какой то либе, но версия с уязвимостями. Указываем принудительно более новую:
-    implementation("org.apache.commons:commons-compress:$apacheCommonsCompress")     // https://mvnrepository.com/artifact/org.apache.commons/commons-compress
+    implementation("org.apache.commons:commons-compress:$apacheCommonsCompressVersion")
     implementation("org.projectlombok:lombok:$lombokVersion")
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
     //implementation("org.liquibase:liquibase-core:4.24.0")
-    //runtimeOnly("org.postgresql:postgresql:42.7.3")      // https://mvnrepository.com/artifact/org.postgresql/postgresql
     implementation("org.thymeleaf:thymeleaf:$thymeleafVersion")
     implementation("org.thymeleaf:thymeleaf-spring5:$thymeleafVersion")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:$springBootVersion")
 
-    runtimeOnly("com.h2database:h2:$h2Db")
+    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
+    runtimeOnly("com.h2database:h2:$h2DbVersion")
 
     annotationProcessor("org.projectlombok:lombok:$lombokVersion")
     annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
