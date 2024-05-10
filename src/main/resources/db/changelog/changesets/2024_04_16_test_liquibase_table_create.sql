b5a762e1033d4dc179823a7d2b283c64774b9ab4
Ilya Slezkin <ilia.slezkin@r1.team>
2024-05-10 12:12:33 +0500

    liquibase_on: liquibase_on

--- src/main/resources/db/changelog/changesets/2024_04_16_test_liquibase_table_create.sql
+++ src/main/resources/db/changelog/changesets/2024_04_16_test_liquibase_table_create.sql
@@ -1,8 +1,8 @@
-create table if not exists test
+create table if not exists test_liquibase_table
 (
     id   bigserial primary key,
     name varchar(50) not null
         constraint uk_1name unique
 );
-alter table test
+alter table test_liquibase_table
     owner to postgres;
--- src/main/resources/db/changelog/changesets/2024_04_17_test_liquibase_table_inserts.sql
+++ src/main/resources/db/changelog/changesets/2024_04_17_test_liquibase_table_inserts.sql
@@ -1,4 +1,4 @@
-insert into test (name)
+insert into test_liquibase_table (name)
 values ('test1'),
        ('test2'),
        ('test3');
