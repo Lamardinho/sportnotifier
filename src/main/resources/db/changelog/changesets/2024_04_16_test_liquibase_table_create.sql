875302c739a514078bde5d96df0681b85212520f
Marcus <slezkin23@gmail.com>
2024-05-12 15:14:44 +0500

    liquibase_ref_and_add_changelogs: liquibase ref and add changelogs

--- src/main/resources/db/changelog/changesets/2024_04_16_test_liquibase_table_create.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists test_liquibase_table
-(
-    id   bigserial primary key,
-    name varchar(50) not null
-        constraint uk_1name unique
-);
-alter table test_liquibase_table
-    owner to postgres;
