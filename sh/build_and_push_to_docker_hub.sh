f0aabc4e8c3a41fa72a554d4e165bbcd044fde24
Ilya Slezkin <ilia.slezkin@r1.team>
2024-05-19 20:35:34 +0500

    develop: NetworkInfoService.java added

--- sh/build_and_push_to_docker_hub.sh
+++ sh/build_and_push_to_docker_hub.sh
@@ -9,12 +9,12 @@ log() {
 }
 
 IMAGE_NAME="slezkin23/sportnotifier"
-VERSION="1.4.1"
+VERSION="1.4.2"
 SPRING_PROFILE="prod"
 
 log "BUILD PROJECT" && ./gradlew clean build
 
-log "MYBUILDER"
+log "BUILDX MY-BUILDER"
 docker buildx create --name mybuilder --use || echo "Docker Buildx уже создан"
 docker buildx inspect mybuilder --bootstrap
 
