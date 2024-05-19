2acec0a499cbbcbb89c8a0ace666837c515dd596
Ilya Slezkin <ilia.slezkin@r1.team>
2024-05-19 13:56:02 +0500

    develop: build_and_push_to_docker_hub.sh refactoring

--- sh/build_and_push_to_docker_hub.sh
+++ sh/build_and_push_to_docker_hub.sh
@@ -12,9 +12,8 @@ SPRING_PROFILE="prod"
 
 log "BUILD PROJECT" && ./gradlew clean build
 log "BUILD AMD" && docker build -t $AMD_TAG --build-arg SPRING_PROFILES_ACTIVE=$SPRING_PROFILE .
-log "PUSH FOR AMD" && docker push $AMD_TAG
-log "BUILD ARM" && docker buildx build --platform linux/arm64  -t $ARM_TAG --build-arg SPRING_PROFILES_ACTIVE=$SPRING_PROFILE .
-log "PUSH ARM" && docker push $ARM_TAG
+log "BUILD ARM" && docker buildx build --platform linux/arm64 -t $ARM_TAG --build-arg SPRING_PROFILES_ACTIVE=$SPRING_PROFILE .
+log "PUSH ALL TAGS" &&  docker push slezkin23/sportnotifier --all-tags  #log "PUSH FOR AMD" && docker push $AMD_TAG && #log "PUSH ARM" && docker push $ARM_TAG
 log "ALL BUILDING COMPLETE"
 
-sleep 100
+sleep 60
