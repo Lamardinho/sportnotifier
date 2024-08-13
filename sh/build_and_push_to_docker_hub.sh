c09f6ee0238b0b0d07f874c31cd0763d46de55d4
Marcus <slezkin23@gmail.com>
2024-08-14 03:20:23 +0500

    develop: переход на java 21

--- sh/build_and_push_to_docker_hub.sh
+++ sh/build_and_push_to_docker_hub.sh
@@ -9,17 +9,21 @@ log() {
 }
 
 IMAGE_NAME="slezkin23/sportnotifier"
-VERSION="1.7.2"
-SPRING_PROFILE="prod"
+VERSION="1.7.6"
+
+error_handler() {
+    sleep 300
+}
+trap 'error_handler' ERR
 
 log "BUILD PROJECT" && ./gradlew clean build
 
 log "BUILDX MY-BUILDER"
-docker buildx create --name mybuilder --use || echo "Docker Buildx уже создан"
+docker buildx rm mybuilder
+docker buildx create --name mybuilder --driver docker-container --use
 docker buildx inspect mybuilder --bootstrap
 
-log "BUILDING ALL TAGS" && docker buildx build --platform linux/amd64,linux/arm64 \
---build-arg SPRING_PROFILES_ACTIVE=$SPRING_PROFILE -t $IMAGE_NAME:latest -t $IMAGE_NAME:$VERSION --push .
+log "BUILDING ALL TAGS" && docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME:latest -t $IMAGE_NAME:$VERSION --push .
 
 log "ALL BUILDING COMPLETE"
 sleep 60
